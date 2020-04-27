2486ecc49cf1a227b14b01a925fcdb840831baff
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-04-27 13:26:29 +0530

    Create simple graphql server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log("Hello GraphQL");
+import { GraphQLServer } from "graphql-yoga";
+
+//  Type definitions (schema)
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+        location: String!
+        bio: String!
+    }
+`;
+
+// Resolvers
+const resolvers = {
+	Query: {
+		hello() {
+			return "Hello GraphQL";
+		},
+		name() {
+			return "Harish Krishnan";
+		},
+		location() {
+			return "Coimbatore";
+		},
+		bio() {
+			return "Full Stack Developer";
+		},
+	},
+};
+
+const server = new GraphQLServer({
+	typeDefs,
+	resolvers,
+});
+
+server.start(() => {
+	console.log("The server is up!");
+});
