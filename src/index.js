5022d063598bdd03bd81514b6c643a4f98ae7a06
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-05-03 09:56:49 +0530

    Add comment typedef

--- src/index.js
+++ src/index.js
@@ -28,18 +28,42 @@ const posts = [
 		title: "NBA Cancelled",
 		body: "NBA cancelled indefinetely because of corona virus",
 		published: true,
+		author: "1",
 	},
 	{
 		id: "2",
 		title: "Formula one restart date",
 		body: "Formula one to be restarted in the end of july!",
 		published: true,
+		author: "2",
 	},
 	{
 		id: "3",
 		title: "IPL postponed indefinetely",
 		body: "IPL postponed until further notice",
 		published: true,
+		author: "3",
+	},
+];
+
+const comments = [
+	{
+		id: "1",
+		text: "Really! Its been a quarter since this has happened",
+		author: "1",
+		post: "1",
+	},
+	{
+		id: "2",
+		text: "Was waiting for this whole year",
+		author: "2",
+		post: "2",
+	},
+	{
+		id: "3",
+		text: "What will I watch without cricket!",
+		author: "3",
+		post: "3",
 	},
 ];
 
@@ -50,21 +74,33 @@ const typeDefs = `
 		post: Post!
 		users(query: String): [User!]!
 		posts(query: String): [Post!]!
+		comments: [Comment!]!
     }
 
     type User {
         id: ID!
         name: String!
         email: String!
-        age: Int
+		age: Int
+		posts: [Post!]!
+		comments: [Comment!]!
     }
 
     type Post {
         id: ID!
         title: String!
         body: String!
-        published: Boolean!
-    }
+		published: Boolean!
+		author: User!
+		comments: [Comment!]!
+	}
+	
+	type Comment {
+		id: ID!
+		text: String!
+		author: User!
+		post: Post!
+	}
 `;
 
 // Resolvers
@@ -107,6 +143,33 @@ const resolvers = {
 					post.body.toLowerCase().includes(query.toLowerCase())
 				);
 			});
+		},
+		comments(parent, args, ctx, info) {
+			return comments;
+		},
+	},
+	Post: {
+		author(parent, args, ctx, info) {
+			return users.find((user) => user.id === parent.author);
+		},
+		comments(parent, args, ctx, info) {
+			return comments.filter((comment) => comment.post === parent.id);
+		}
+	},
+	User: {
+		posts(parent, args, ctx, info) {
+			return posts.filter((post) => post.author === parent.id);
+		},
+		comments(parent, args, ctx, info) {
+			return comments.filter((comment) => comment.author === parent.id);
+		},
+	},
+	Comment: {
+		author(parent, args, ctx, info) {
+			return users.find((user) => user.id === parent.author);
+		},
+		post(parent, args, ctx, info) {
+			return posts.find((post) => post.id === parent.post);
 		},
 	},
 };
